90af79ff44bd043caa68c1e3e29d18ce3d67f652
S150405 <joakim.schweizer@nav.bo>
2019-01-25 15:28:04 +0100

    Mere testing av variabler

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,11 +1,21 @@
 public class FirstJavaProgram {
-    public static String classVar1="First class variable";
-    public static String classVar2="Second class variable";
+    private static String classVar1="First class variable";
+    private static String classVar2="Second class variable";
+    public String instanceVar="An instance variable";
+
+    public void firstMethod(){
+        String localVar="A local variable inside a method";
+
+        System.out.println("Printing text inside a method, and then the var:");
+        System.out.println(localVar);
+    }
+
     public static void main(String args[]){
         FirstJavaProgram var1 = new FirstJavaProgram();
         FirstJavaProgram var2 = new FirstJavaProgram();
         FirstJavaProgram var3 = new FirstJavaProgram();
 
+        System.out.println(classVar1+" Check");
         System.out.println(var1.classVar1);
         System.out.println(var2.classVar1);
         System.out.println(var3.classVar1);
@@ -13,7 +23,12 @@ public class FirstJavaProgram {
         System.out.println(var2.classVar2);
         System.out.println(var3.classVar2);
 
-        var3.classVar2 = "3213";
+        System.out.println(var1.instanceVar);
+        System.out.println(var2.instanceVar);
+
+        var1.instanceVar = "A changed instance variable";
+
+        var3.classVar2 = "Second class var changed";
 
         System.out.println("--------------------");
 
@@ -23,5 +38,12 @@ public class FirstJavaProgram {
         System.out.println(var1.classVar2);
         System.out.println(var2.classVar2);
         System.out.println(var3.classVar2);
+
+        System.out.println(var1.instanceVar);
+        System.out.println(var2.instanceVar);
+
+        System.out.println("Here comes the method:");
+        var1.firstMethod();
+        System.out.println(var1.instanceVar);
     }
 }
