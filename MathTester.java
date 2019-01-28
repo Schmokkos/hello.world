2238e927b27c0aff59b50222aba12a26cb359834
S150405 <joakim.schweizer@nav.bo>
2019-01-28 13:55:53 +0100

    Oppdatert matte app med hvor mange spørsmål. Prøver å legge inn karakter også, men sliter med prosenten her...

--- MathTester.java
+++ MathTester.java
@@ -2,29 +2,50 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class MathTester {
+    private static int correctAnswers = 0;
+    private static int numberOfQuestions;
 
-    public static void main(String[] args){
+    static Random rnum = new Random();
+    static Scanner scan = new Scanner(System.in);
 
-        System.out.println("Welcome to Math Tester 1.0 :)");
-        System.out.println("*******************");
-
-        Scanner scan = new Scanner(System.in);
-        Random rnum = new Random();
-
-        int num1 = rnum.nextInt(200);
-        int num2 = rnum.nextInt(200);
+    private static void question(){
+        int num1 = rnum.nextInt(20);
+        int num2 = rnum.nextInt(20);
 
         System.out.println("What is: "+num1+"+"+num2+" ?");
         int result1 = scan.nextInt();
 
         if (result1 == num1+num2){
-            System.out.print("Correct!");
+            System.out.println("Correct!");
+            correctAnswers++;
         }
         else {
-            System.out.print("Wrong! Correct answer was: "+(num1+num2));
+            System.out.println("Wrong! Correct answer was: "+(num1+num2));
+        }
+    }
+    private static void grade(){
+        float percent = correctAnswers / numberOfQuestions;
+        System.out.print(percent);
+        //Hvorfor funker ikke dette??
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Welcome to Math Tester 1.0 :)");
+        System.out.println("*******************");
+        System.out.print("Enter the number of questions you want: ");
+        numberOfQuestions = scan.nextInt();
+
+        int i;
+        for (i = 1; i <= numberOfQuestions; i++){
+            MathTester.question();
         }
 
         // Closing Scanner after the use
         scan.close();
+
+        System.out.println("----------------------");
+        System.out.println("Correct answers: "+correctAnswers+"/"+numberOfQuestions);
+        MathTester.grade();
     }
 }
